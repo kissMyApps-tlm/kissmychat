81f64bdc6e11cb5e85bdeb2769bc41c99883d1a5
Vladlislav Leonchyk <u.liavonchyk@tontilagunamobile.com>
2025-03-06 18:12:44 +0100

    fix(fe): update branding details for KissMyChat
    Renames LobeChat to KissMyChat, updates organizational name to KissMyApps, and adds new branding URLs including logo, help, privacy, and terms pages.

--- src/const/branding.ts
+++ src/const/branding.ts
@@ -2,15 +2,16 @@
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
 
-export const LOBE_CHAT_CLOUD = 'LobeChat Cloud';
+export const LOBE_CHAT_CLOUD = 'KissMyChat Cloud';
 
-export const BRANDING_NAME = 'LobeChat';
-export const BRANDING_LOGO_URL = '';
+export const BRANDING_NAME = 'KissMyChat';
+export const BRANDING_LOGO_URL =
+  'https://cdn2.spatial.io/assets/v1/thumbnails/650248e11c72341c21a7f66f/customLogo/r/118f1a6a9ad726fdaa849a2afa65d7001fc0148199a59badb1626626f3357519/1718464913';
 
-export const ORG_NAME = 'LobeHub';
+export const ORG_NAME = 'KissMyApps';
 
 export const BRANDING_URL = {
-  help: undefined,
-  privacy: undefined,
-  terms: undefined,
+  help: 'https://tonti-laguna-mobile.slack.com/team/U04P96WJTHD',
+  privacy: 'https://www.kissmyapps.tech/privacy-policy',
+  terms: 'https://www.kissmyapps.tech/terms-of-use',
 };
