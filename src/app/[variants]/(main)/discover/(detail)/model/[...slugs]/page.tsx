690a81c227b393dd428775274438fa369f72e719
Vladlislav Leonchyk <u.liavonchyk@tontilagunamobile.com>
2025-03-12 18:00:46 +0100

    feat: urk trans

--- src/app/[variants]/(main)/discover/(detail)/model/[...slugs]/page.tsx
+++ src/app/[variants]/(main)/discover/(detail)/model/[...slugs]/page.tsx
@@ -52,8 +52,8 @@ export const generateMetadata = async (props: DiscoverPageProps) => {
   return {
     authors: [
       { name: meta.title },
-      { name: 'LobeHub', url: 'https://github.com/lobehub' },
-      { name: 'LobeChat', url: 'https://github.com/lobehub/lobe-chat' },
+      // { name: 'LobeHub', url: 'https://github.com/lobehub' },
+      // { name: 'LobeChat', url: 'https://github.com/lobehub/lobe-chat' },
     ],
     webpage: {
       enable: true,
