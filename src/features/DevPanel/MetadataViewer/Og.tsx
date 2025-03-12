65aa3352ddc0ecc1c759bbf4abbfc0314af643dc
Vladlislav Leonchyk <u.liavonchyk@tontilagunamobile.com>
2025-03-12 18:00:46 +0100

    feat: urk trans

--- src/features/DevPanel/MetadataViewer/Og.tsx
+++ src/features/DevPanel/MetadataViewer/Og.tsx
@@ -47,7 +47,7 @@ const MetaData = memo(() => {
                   zIndex: 10,
                 }}
               >
-                lobehub.com
+                KissMyApps.tech
               </div>
               <Image
                 alt={'og'}
