51c718d75c1f2ef94f87c221d7b7a199e77df2e0
Vladlislav Leonchyk <u.liavonchyk@tontilagunamobile.com>
2025-09-10 11:54:55 +0200

    feat: combined changes from my fork (squashed 33 commits)

--- src/app/[variants]/(main)/discover/(detail)/model/[...slugs]/page.tsx
+++ src/app/[variants]/(main)/discover/(detail)/model/[...slugs]/page.tsx
@@ -44,8 +44,8 @@ export const generateMetadata = async (props: DiscoverPageProps) => {
   return {
     authors: [
       { name: displayName || identifier },
-      { name: 'LobeHub', url: 'https://github.com/lobehub' },
-      { name: 'LobeChat', url: 'https://github.com/lobehub/lobe-chat' },
+      // { name: 'LobeHub', url: 'https://github.com/lobehub' },
+      // { name: 'LobeChat', url: 'https://github.com/lobehub/lobe-chat' },
     ],
     webpage: {
       enable: true,
